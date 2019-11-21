3f4631011ea3703729b9fd7fb6bd0ac26093e8fd
Omer Katan <omerkatan@live.com>
2019-11-21 15:01:19 -0800

    html writing function working

--- process.js
+++ process.js
@@ -3,21 +3,37 @@ const util = require('util');
 
 const readFileAsync = util.promisify(fs.readFile);
 
-
+function generateHTML(data) {
+    return `<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Resume</title>    
+        </head>
+    
+        <body>
+            <img id="profilePic" src="${data[0].profilePic}" alt="ProfilePic">
+        </body>
+    </html>`;
+}
 
 
 
 function readData() {
     readFileAsync("userInfo.json", "utf8").then(function(rawdata) {
 
-        const getData = JSON.parse(rawdata);
+        const data = JSON.parse(rawdata);
 
-        
+        // generateHTML(data);
+
+        fs.writeFile('index.html', generateHTML(data), function(err) {
+            if(err) throw err;
+
+            console.log('html file written');
+        })
 
     });
-
 }
 
 module.exports = {
-    read_createHTML: readData
+    readData: readData
 }
